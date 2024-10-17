284855544ef8e8d45e40f0e97ec126f1833e16ca
senuri balasooriya <senuribalasuriya@gmail.com>
2024-10-18 03:11:18 +0530

    ok

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,8 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import logo from '/logo.png?url';
-import { FiPhoneCall } from "react-icons/fi"
-
-
+import { Link } from 'react-router-dom'; // If you're using React Router
 
 const Navbar = () => {
   const [isSticky,setSticky] = useState(false);
@@ -22,107 +19,47 @@ const Navbar = () => {
       window.removeEventListener("scroll",handleScroll); 
     }
   }, [])
-
-    const navItems = (
-        <>
-        <li><a href='/'>Home</a></li>
-      <li tabIndex={0}>
-        <details>
-          <summary>Menu</summary>
-          <ul className="p-2">
-            <li><a>All</a></li>
-            <li><a>Salad</a></li>
-            <li><a>Pizza</a></li>
-          </ul>
-        </details>
-      </li>
-      <li tabIndex={0}>
-        <details>
-          <summary>Services</summary>
-          <ul className="p-2">
-            <li><a>Online Order</a></li>
-            <li><a>Table Booking</a></li>
-            <li><a>Order Tracking</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Offers</a></li>
-        </>
-    )
   return (
-    <header className='container fixed top-0 left-0 right-0 mx-auto transition-all duration-300 ease-in-out max-w-screen-2xl'>
-        <div className={`navbar xl:px-24 ${isSticky ? "shadow-md bg-base-100 transition-all duration-300 ease-in-out":"bg-purple-50"}`}>
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="w-5 h-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path  
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16" />
-        </svg>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        {navItems}
-      </ul>
-    </div>
-    <a href='/'>
-        <img src="./logo.png" alt="logo" className='w-32 h-auto'/>
-    </a>
-  </div>
-  <div className="hidden navbar-center lg:flex">
-    <ul className="px-1 menu menu-horizontal">
-      {navItems}
-    </ul>
-  </div>
-  <div className=" navbar-end">
-    {/* search*/}
-    <button className="items-center justify-center hidden mr-3 btn btn-ghost btn-circle lg:flex">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="w-5 h-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-      </svg>
-    </button>
-    {/*cart */}
-    <div tabIndex={0} role="button" className="items-center justify-center hidden mr-3 lg:flex btn btn-ghost btn-circle">
-        <div className="indicator">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-5 h-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-          </svg>
-          <span className="badge badge-sm indicator-item">0</span>
+    <header className="container fixed top-0 left-0 right-0 mx-auto transition-all duration-300 ease-in-out max-w-screen-2xl">
+      <div className={`navbar xl:px-12 ${isSticky ? "shadow-md bg-base-100 transition-all duration-300 ease-in-out":"bg-green-50"}`}>
+        <div className="container mx-auto">
+          <div className="flex items-center justify-between">
+            {/* Logo Section */}
+            <div className="logo">
+              <Link to="/" className="standard-logo">
+                <img src="/public/logo.jpg" alt="logo" className="h-24 w-36" />
+              </Link>
+            </div>
+
+            {/* Navigation Menu */}
+            <nav className="menu">
+              <ul className="flex space-x-6">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/Recipe">Recipe Finder</Link></li>
+                <li><Link to="/Tips">Storage Tips</Link></li>
+                <li><Link to="/Aboutus">About Us</Link></li>
+                <li><Link to="/Contactus">Contact Us</Link></li>
+              </ul>
+            </nav>
+
+            {/* Support Button */}
+            <div className="header-btn">
+              <Link to="/contact" className="header-btn-a">
+                Support
+              </Link>
+            </div>
+
+            {/* Mobile Menu Toggle */}
+            <div className="mobile-menu-bar lg:hidden text-end">
+              <button className="mobile-menu-toggle-btn">
+                <i className="fal fa-bars"></i>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    {/* contact */}
-    <a className="flex items-center gap-2 px-6 text-white bg-purple-900 rounded-full btn"><FiPhoneCall />Contact</a>
-  </div>
-</div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

d6c8688264761ce9cd1a0186e3ae035c7e5407a6
senuri balasooriya <senuribalasuriya@gmail.com>
2024-10-19 19:04:49 +0530

    ok

--- my-project/src/layout/Main.jsx
+++ my-project/src/layout/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import "../App.css"
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/footer'
 
 
 
@@ -10,6 +11,7 @@ const Main = () => {
     <div>
         <Navbar/>
         <Outlet/>
+        <Footer/>
     </div>
     
   )
